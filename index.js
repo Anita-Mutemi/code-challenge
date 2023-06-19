cb150814cbcfde21c32c3c22d50018beeb1b7c61
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-19 03:23:01 +0300

    added the first loop

---
+++ index.js
@@ -0,0 +1,9 @@
+// Student Grade Generator (Toy Problem)
+// Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+// A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+for (let student in studentMarks) {
+    let grade;
+    let percentage = studentMarks[student];
+
+    if (percentage >=79 && percentage <= 100)
+}
